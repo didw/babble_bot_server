ce0f7940e6977553f14977333de5bdc18b446f2e
Jongyeol Yang <yangjy0113@gmail.com>
2023-09-18 20:28:05 +0900

    mc

--- chat_service.py
+++ chat_service.py
@@ -6,6 +6,13 @@ def get_gpt_response(messages):
         openai_config = json.load(f)
         
     openai.api_key = openai_config["api_key"]
+    
+    system_prompt = """
+    you are chatbot and you are talking to a human.
+    you answer politely and try to help the human.
+    response sentences consist by 5 ~ 20 words.
+    """
+    messages[0]["content"] = system_prompt
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
